2c21a5aab951cced805d3c6efeb47414880f14c8
mschick <mschick@novomind.com>
2023-01-31 17:58:40 +0100

    day031

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 let age = 40;
 let userName = 'Mela';
 let hobbies = ['Sports', 'Reading', 'Programming'];
-let job = {title: 'Software Developer', place: 'Hamburg'};
+let job = {
+  title: 'Software Developer', 
+  place: 'Hamburg', 
+  salary: 60000
+};
+
+let adultYears;
+
+function calculateAdultYears(userAge) {
+  return userAge-18;
+}
+
+adultYears = calculateAdultYears(age);
+alert(adultYears);
