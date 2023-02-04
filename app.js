2efcd7a6ea35d8ed7a2a52c4af62880ed6315b3e
mela Letics <mschick@mailbox.org>
2023-02-04 13:57:26 +0100

    day035

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-//console.dir(document)
+let paragraphElement = document.querySelector('p');
+let inputField = document.querySelector('input');
 
-//document.body.children[1].children[0].href="https:\\www.google.de";
+function changeParagraphText() {
+    paragraphElement.textContent = 'Clicked';
+}
+
+function retrieveUserInput(event) {
+    console.log(event);
+    console.log(event.target.value);
+}
+
+paragraphElement.addEventListener('click', changeParagraphText);
+inputField.addEventListener('input', retrieveUserInput);
+
+
+/*
+console.dir(document)
+
+document.body.children[1].children[0].href="https:\\www.google.de";
 
 let anchorElement = document.getElementById('external-link');
 anchorElement.href = "https:\\www.google.de";
@@ -13,3 +30,15 @@ newAnchor.href = "https:\\www.google.de";
 newAnchor.textContent = 'This also leads to google';
 let paragraph = document.querySelector('p');
 paragraph.appendChild(newAnchor);
+
+//remove an element
+let firstH1Element = document.querySelector('h1');
+firstH1Element.remove();
+
+// move elements
+paragraph.parentElement.append(paragraph);
+
+//innerHTML
+console.log(paragraph.innerHTML);
+paragraph.innerHTML= 'Hi! This is <strong> important!</strong>';
+*/
