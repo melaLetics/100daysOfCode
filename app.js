842b0b6e95070fc89cbc0f9a8b9abf555cb8b133
mschick <mschick@novomind.com>
2023-02-07 18:03:35 +0100

    day038

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const remainingChars = document.getElementById('remaining-chars');
 function calcChars() {
     remainingChars.innerText = inputElement.maxLength- inputElement.value.length;
 
-    if (inputElement.value.length >= inputElement.maxLength - 10){
+    if (inputElement.value.length === inputElement.maxLength){
+        remainingChars.classList.add('error');
+        inputElement.classList.add('error');
+    } else if (inputElement.value.length >= inputElement.maxLength - 10){
+        remainingChars.classList.remove('error');
+        inputElement.classList.remove('error');
         remainingChars.classList.add('warning');
         inputElement.classList.add('warning');
     } else {
