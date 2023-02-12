3a2bc8d481b6df69217891f8ac13e658574d38cf
mela Letics <mschick@mailbox.org>
2023-02-12 16:50:22 +0100

    day048

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-function handleRequest(request, response) {
+const express = require('express');
 
-    if (request.url === '/currenttime') {
-        response.statusCode = 200;
-        response.end('<h1>' + new Date().toISOString() + '</h1>');
-    } else if (request.url === '/'){
-        response.statusCode = 200;
-        response.end('<h1>Hello World!</h1>');
-    }
-}
+const app = express();
+app.use(express.urlencoded({extended: false}));
 
-const server =http.createServer(handleRequest);
-server.listen(3000);
+app.get('/currenttime', function(request, response){
+    response.send('<h1>' + new Date().toISOString() + '</h1>')
+});
+
+app.get('/', function(request, response){
+    response.send('<form action="/store-user" method="POST"><label>Your name</label><input type="text" name="username"><button>Submit</button></form>');
+});
+
+app.post('/store-user', function(request, response){
+    const username = request.body.username;
+
+    const filePath = path.join(__dirname, 'data', 'user.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+    existingUsers.push(username);
+    
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
+    response.send('<h1>Username stored.</h1>');
+});
+
+app.listen(3000);
