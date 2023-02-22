37a5b2a112e12d995e8d3a008ae56ac6f6102ad3
mela Letics <mschick@mailbox.org>
2023-02-22 11:24:21 +0100

    day050

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', function(request, response) {
-    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-    response.sendFile(htmlFilePath);
+    response.render('index');
 })
 
 app.get('/restaurants', function(request, response){
-    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
-    response.sendFile(htmlFilePath);
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+    
+    response.render('restaurants', {
+        numberOfRestaurants: storedRestaurants.length ,
+        restaurants: storedRestaurants
+    });
 });
 
 app.get('/recommend', function(request, response){
-    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
-    response.sendFile(htmlFilePath);
+    response.render('recommend');
+});
+
+app.post('/recommend', function(request, response){
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+    storedRestaurants.push(request.body);
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+    response.redirect('/confirm');
 });
 
 app.get('/about', function(request, response){
-    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
-    response.sendFile(htmlFilePath);
+    response.render('about');
 })
 
 app.get('/confirm', function(request, response){
-    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
-    response.sendFile(htmlFilePath);
+    response.render('confirm');
 });
 
 app.listen(3000);
