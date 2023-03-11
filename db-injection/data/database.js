12facb439fd9063c493aad93e14e384be32179a6
mela Letics <mschick@mailbox.org>
2023-03-11 09:59:20 +0100

    day074

---
+++ db-injection/data/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  database: 'security',
+  user: 'root',
+  password: 'my-password',
+  multipleStatements: true
+})
+
+module.exports = pool;
