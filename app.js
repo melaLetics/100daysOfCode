a4ed00f833804db23044dabf653ac7e0bbdd36dd
mela Letics <mschick@mailbox.org>
2023-03-14 16:50:05 +0100

    day079

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
 
 app.use(authRoutes);
 
