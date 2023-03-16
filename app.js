6ec1afc6c11870e6b592f9bd90468bbdf1edf2f8
mela Letics <mschick@mailbox.org>
2023-03-16 08:06:37 +0100

    day081

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 const express = require('express');
+const csrf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
 const authRoutes = require('./routes/auth.routes');
+const csrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handlers');
 
 const app = express();
 
@@ -11,8 +16,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+app.use(csrfTokenMiddleware);
 
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
 .then(() => {
